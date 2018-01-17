8be68085ed35ca0b1fdd7f7fab6956b19c367eec
Harshit Pareek <hp1014@nyu.edu>
2018-01-17 10:50:00 -0500

    feat(refactor)

--- src/app.js
+++ src/app.js
@@ -1,15 +1,7 @@
 import express from "express";
-import fs from "fs";
-import moment from "moment-timezone";
-import _ from "lodash";
-import sendgrid from "sendgrid";
-
 const app = express();
 
 const port = process.env.PORT || 3000;
-
-const arr = [1,2,3,4,5];
-console.log(arr.map(value => value * 2));
 
 app.get("/", (req, res) => {
   res.send("<h2> Welcome to Node </h2>");
@@ -18,3 +10,43 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("app started at port: "+port);
 });
+
+// const verticalPrinting = (input) => {
+//   let sortedArray = input.split(" ").sort(); // spliting and sorting the array
+//   let maxLengthArray = []; // array will contain the maximum length word in columns 
+//   let outputArray = []; // output array will be printed on the console
+//   let columnArray = [, , ,]; // column will contain the elements for the perticular column
+//   // positioning the elements in the sorted array
+//   for(let i=0;i<sortedArray.length;i++) {
+//     let position = Math.min(Math.floor((i/(Math.floor(sortedArray.length/3)))), 2);
+//     // counting the rows in the output array
+//     if(position === 2) {
+//       outputArray.push("");
+//     }
+//     if(columnArray[position] === undefined || columnArray[position] === null) {
+//       columnArray[position] = [];
+//       columnArray[position].push(sortedArray[i]);
+//       maxLengthArray[position] = sortedArray[i].length;
+//     }
+//     else {
+//       columnArray[position].push(sortedArray[i]);
+//       maxLengthArray[position] = Math.max(maxLengthArray[position], sortedArray[i].length);
+//     }
+//   }
+//   // printing on the console by using the output array
+//   for(let i=0;i<maxLengthArray.length;i++) {
+//     for(let j=0;j<=maxLengthArray[i];j++) {
+//       for(let k=0;k<outputArray.length;k++) {
+//         if(k >= columnArray[i].length || j >= columnArray[i][k].length) {
+//           outputArray[k] += " ";
+//         }
+//         else {
+//           outputArray[k] += columnArray[i][k][j];
+//         }
+//       }
+//     }
+//   }
+//   return outputArray.join("\n");
+// }
+
+// console.log(verticalPrinting("cat caterpillar pie frog elephant pizza"));
